dfa20d7cacb400bd5ec5df38b02e3f776e6f68ad
Onur Taşkıran <onurtaskiran95@gmail.com>
2018-05-26 00:18:07 +0300

    first commit

---
+++ controllers/api/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\Randevu\controllers\api;
+
+
+/**
+ * Default controller for the `Randevu` module
+ */
+class DefaultController extends \kouosl\base\controllers\api\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
---
+++ controllers/console/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\Randevu\controllers\console;
+
+
+/**
+ * Default controller for the `Randevu` module
+ */
+class DefaultController extends \kouosl\base\controllers\console\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
